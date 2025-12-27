028d7ad3cdf23ac96d87e6ed54e46efe1f4f3246
OEvortex <abhat8283@gmail.com>
2025-12-27 23:04:06 +0530

    refactor: update .gitignore and remove unused lol.py file
    Signed-off-by: OEvortex <abhat8283@gmail.com>

--- lol.py
+++
@@ -1,7 +0,0 @@
-from webscout.AIauto import AUTO
-
-client = AUTO(print_provider_info=True)
-ai = client.chat("Hello, how can I assist you today?", stream=True)
-while True:
-    for message in ai:
-        print(message, end="", flush=True)
